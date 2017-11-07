ab7f85b85bc722fa071bd909a7ae0358acb70728
Турсенев Антон <a.tursenev@corp.mail.ru>
2017-11-07 09:22:33 +0300

    added robots and favicon

--- config/html.js
+++ config/html.js
@@ -12,7 +12,8 @@ module.exports = function (env, options) {
     appMountId: 'app',
     mobile: true,
     lang: 'en-US',
-    alwaysWriteToDisk: true
+    alwaysWriteToDisk: true,
+    favicon: '/favicon.ico'
   };
 
   if (env === 'build') {
@@ -29,37 +30,104 @@ module.exports = function (env, options) {
       removeStyleTypeAttributes: true
     };
 
-    config.meta = [
-      {
-        'http-equiv': 'Content-Security-Policy',
-        content: "default-src 'self' ; img-src 'self' data: ; font-src 'self' data: ; object-src 'none' ; child-src 'none' ; frame-src 'none' ; form-action 'self' ; upgrade-insecure-requests; block-all-mixed-content; base-uri https://rikishi.info/"
-      },
-      {
-        'http-equiv': 'X-XSS-Protection',
-        content: '1;mode=block'
-      },
-      {
-        'http-equiv': 'Strict-Transport-Security',
-        content: 'max-age=31536000; includeSubDomains; preload'
-      },
-      {
-        'http-equiv': 'X-Frame-Options',
-        content: 'DENY'
-      },
-      {
-        'http-equiv': 'X-Content-Type-Options',
-        content: 'nosniff'
-      },
-      {
-        name: 'description',
-        content: 'A better default template for html-webpack-plugin.'
-      },
-      {
-        name: 'google',
-        content: 'notranslate'
-      }
-    ];
+    config.meta = meta();
+    config.links = links();
   }
 
   return config;
 };
+
+function links () {
+  return [
+    '57x57',
+    '60x60',
+    '72x72',
+    '76x76',
+    '114x114',
+    '120x120',
+    '144x144',
+    '152x152',
+    '180x180'
+  ].map(function (size) {
+    return {
+      href: '/apple-icon-' + size + '.png',
+      rel: 'apple-touch-icon',
+      sizes: size
+    };
+  }).concat([
+    '32x32',
+    '96x96',
+    '16x16'
+  ].map(function (size) {
+    return {
+      href: '/favicon-' + size + '.png',
+      rel: 'icon',
+      sizes: size,
+      type: 'image/png'
+    };
+  }), {
+    href: '/android-icon-192x192.png',
+    rel: 'icon',
+    sizes: '192x192',
+    type: 'image/png'
+  });
+}
+
+function meta () {
+  return [
+    {
+      'http-equiv': 'Content-Security-Policy',
+      content:
+        "default-src 'self';" +
+        "img-src 'self' data:;" +
+        "font-src 'self' data:;" +
+        "object-src 'none';" +
+        "child-src 'none';" +
+        "frame-src 'none';" +
+        "form-action 'self';" +
+        "upgrade-insecure-requests;" +
+        "block-all-mixed-content;" +
+        "base-uri https://rikishi.info/"
+    },
+    {
+      'http-equiv': 'X-XSS-Protection',
+      content: '1;mode=block'
+    },
+    {
+      'http-equiv': 'Strict-Transport-Security',
+      content: 'max-age=31536000; includeSubDomains; preload'
+    },
+    {
+      'http-equiv': 'X-Frame-Options',
+      content: 'DENY'
+    },
+    {
+      'http-equiv': 'X-Content-Type-Options',
+      content: 'nosniff'
+    },
+    {
+      name: 'description',
+      content: 'A better default template for html-webpack-plugin.'
+    },
+    {
+      name: 'google',
+      content: 'notranslate'
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#ffffff'
+    },
+    {
+      name: 'msapplication-TileImage',
+      content: '/ms-icon-144x144.png'
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff'
+    }
+  ];
+}
+
+/*
+<link rel="manifest" href="/manifest.json">
+*/
