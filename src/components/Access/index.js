965a6d1f19b0481ca2be0f2a86b830c5820fd0f9
Турсенев Антон <a.tursenev@corp.mail.ru>
2017-11-15 14:42:09 +0300

    access

---
+++ src/components/Access/index.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import Footer from '../Footer';
+
+import style from './index.css';
+
+export default function Access () {
+
+  return (
+    <div className={style.access}>
+      <div className={style.accessContent}>
+        <h1 className={style.accessHeader}>403</h1>
+        <p>
+          <Link to="/" className={style.accessBack}>Go to home</Link>
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,9 +5,23 @@ import {
 } from 'react-router-dom';
 
 import About from '../About';
+import Access from '../Access';
 import NoMatch from '../NoMatch';
 
 import style from './index.css';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to={{
+        pathname: '/access',
+        state: { from: props.location }
+      }} />
+    )
+  )} />
+);
 
 export default function App () {
   // require.ensure([], (require) => {
@@ -21,7 +35,9 @@ export default function App () {
         <Switch>
           <Route exact path="/" component={About} />
           <Route path="/about" component={About} />
-          <Route component={NoMatch}/>
+          <Route path="/access/:token?" component={Access} />
+          <PrivateRoute path="/test" component={About} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     </Router>
