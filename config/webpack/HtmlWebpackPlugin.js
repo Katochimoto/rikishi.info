0a1557f531d225a9ff2cfe92be948c9925b48227
Katochimoto <bufpost@yandex.ru>
2018-07-30 19:24:49 +0700

    qr code

--- config/webpack/HtmlWebpackPlugin.js
+++ config/webpack/HtmlWebpackPlugin.js
@@ -44,7 +44,7 @@ module.exports = function (config, options) {
         "default-src 'self';" +
         "connect-src 'self' https://pgp.mit.edu;" +
         "script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com;" +
-        "img-src 'self' https://www.google-analytics.com;" + // data: https://s.gravatar.com
+        "img-src 'self' data: https://www.google-analytics.com;" + // data: https://s.gravatar.com
         // "font-src 'self' data:;" +
         "object-src 'none';" +
         "child-src 'none';" + // deprecated
